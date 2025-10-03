a48445f9e5e325c606b408f68122b596a1a86285
Kendyn Fredieu <kendyn.fredieu@gmail.com>
2025-10-03 11:29:47 -0400

    Complete join_modular

--- threads-cv/join_modular.c
+++ threads-cv/join_modular.c
@@ -1,59 +1,63 @@
+// join_modular.c  -- modular sync with plain pthreads
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-#include "common.h"
-#include "common_threads.h"
-
-//
-// Simple sync "object"
-//
 
 typedef struct {
-    pthread_cond_t c;
     pthread_mutex_t m;
+    pthread_cond_t  c;
     int done;
 } synchronizer_t;
 
-synchronizer_t s;
-
-void sync_init(synchronizer_t *s) {
+static void sync_init(synchronizer_t *s) {
+    pthread_mutex_init(&s->m, NULL);   // two args: (&mutex, attr)
+    pthread_cond_init(&s->c, NULL);    // two args: (&cond,  attr)
     s->done = 0;
-    Mutex_init(&s->m);
-    Cond_init(&s->c);
 }
 
-void sync_signal(synchronizer_t *s) {
-    Mutex_lock(&s->m);
+static void sync_wait(synchronizer_t *s) {
+    pthread_mutex_lock(&s->m);
+    while (!s->done) {                 // protect predicate with the mutex
+        pthread_cond_wait(&s->c, &s->m);
+    }
+    pthread_mutex_unlock(&s->m);
+}
+
+static void sync_signal(synchronizer_t *s) {
+    pthread_mutex_lock(&s->m);
     s->done = 1;
-    Cond_signal(&s->c);
-    Mutex_unlock(&s->m);
+    pthread_cond_signal(&s->c);
+    pthread_mutex_unlock(&s->m);
 }
 
-void sync_wait(synchronizer_t *s) {
-    Mutex_lock(&s->m);
-    while (s->done == 0) 
-	Cond_wait(&s->c, &s->m); 
-    s->done = 0; // reset for next use
-    Mutex_unlock(&s->m);
+static void sync_destroy(synchronizer_t *s) {
+    pthread_mutex_destroy(&s->m);
+    pthread_cond_destroy(&s->c);
 }
 
-
-//
-// Main threads
-//
-
-void *child(void *arg) {
-    printf("child\n");
+static void* child(void *arg) {
+    synchronizer_t *s = (synchronizer_t*)arg;
+    printf("child: begin\n");
     sleep(1);
-    sync_signal(&s);
+    printf("child: signal\n");
+    sync_signal(s);
     return NULL;
 }
-int main(int argc, char *argv[]) {
-    pthread_t p;
-    printf("parent: begin\n");
+
+int main(void) {
+    synchronizer_t s;
+    pthread_t t;
+
     sync_init(&s);
-    Pthread_create(&p, NULL, child, NULL);
+
+    printf("parent: begin\n");
+    pthread_create(&t, NULL, child, &s);
+
+    printf("parent: waiting\n");
     sync_wait(&s);
+
+    pthread_join(t, NULL);
+    sync_destroy(&s);
     printf("parent: end\n");
     return 0;
 }
